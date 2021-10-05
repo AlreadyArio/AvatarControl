0acab4b1d9bfebce0b5e69b1ad1fa2d22fcec331
Trisxcal <55791489+Trisxcal@users.noreply.github.com>
2021-10-05 18:14:23 +0200

    Final Update

--- AvatarControlModule.lua
+++ AvatarControlModule.lua
@@ -18,55 +18,7 @@ local ACS = {
 		Right = {-32.5, 5, 40},
 		RightOR = {0, -20, 0}
 	},
-
-	Instruments = {
-		["Singer"] = {
-			Idle = "rbxassetid://6539543263",
-			Playing = "rbxassetid://6539372582",
-		},
-		["Cello"] = {
-			Idle = "rbxassetid://6536593350",
-			Playing = "rbxassetid://6535911435",
-		},
-
-		["Saxophone"] = {
-			Idle = "rbxassetid://6543999194",
-			Playing = "rbxassetid://6543940416",
-		},
-	},
-
-	Songs = {
-		["France"] = {
-			game.Workspace.Songs.France,
-			{"Singer", game.Workspace.Musicians.Singer, "Front"},
-			{"Saxophone", game.Workspace.Musicians.Saxophone, "Right"},
-			{"Cello", game.Workspace.Musicians.Cello, "Left"},
-		}
-	}
-
 }
-
-local function PlayInstrumentAnimation(Humanoid, Playing, Idle, SpeedTrue, SpeedFalse, Bool)
-
-	local LoadedPlaying = Humanoid:LoadAnimation(Playing)
-	local LoadedIdle = Humanoid:LoadAnimation(Idle)
-
-	if Bool then
-		LoadedIdle:AdjustWeight(1)
-		LoadedPlaying:AdjustWeight(10)
-		LoadedPlaying:Play()
-		LoadedPlaying.Looped = true
-		LoadedIdle:Stop()
-		LoadedPlaying:AdjustSpeed(SpeedTrue)
-	else
-		LoadedIdle:AdjustWeight(10)
-		LoadedPlaying:AdjustWeight(1)
-		LoadedPlaying:Stop()
-		LoadedIdle:Play()
-		LoadedIdle.Looped = true
-		LoadedPlaying:AdjustSpeed(SpeedFalse)
-	end
-end
 
 function ACS:RemoveAvatar(Model)
 	assert(Model:IsA("Model") and Model.PrimaryPart, "Model must be a valid Player Model")
@@ -82,7 +34,7 @@ end
 
 function ACS:AddAvatar(Model, Location)
 	assert(Model:IsA("Model") and Model.PrimaryPart, "Model must be a valid Player Model")
-	assert(type(Location) == "string", "Input must be a valid string")
+	assert(type(Location) == "string", "Value must be a valid string")
 	assert(Location == "Front" or Location == "Back" or Location == "Left" or Location == "Right", "Input must be a valid location type")
 
 	if Model then
@@ -93,40 +45,37 @@ function ACS:AddAvatar(Model, Location)
 			Model:SetPrimaryPartCFrame(Model.PrimaryPart.CFrame * CFrame.Angles(math.rad(ACS.Positions[Location.."OR"][1]), math.rad(ACS.Positions[Location.."OR"][2]), math.rad(ACS.Positions[Location.."OR"][3])))
 		end
 	end
-
+	
 end
 
-function ACS:PlayAvatarAnimation(Instrument, Bool)
-	assert(Instrument == "Cello" or Instrument == "Saxophone" or Instrument == "Singer", "Input must be a valid instrument type")
+function ACS:PlayAvatarAnimation(Model, SpeedTrue, SpeedFalse, Bool)
+	assert(Model:IsA("Model") and Model.PrimaryPart, "Model must be a valid Player Model")
+	assert(type(SpeedFalse) == "number" and type(SpeedTrue) == "number" , "Value must be a valid number")
+	assert(type(Bool) == "boolean" or Bool == nil , "Value must be a valid boolean")
 
-	local Model = game.Workspace.Musicians:FindFirstChild(Instrument)
 	local AnimationsFolder = Model:FindFirstChild("Animations")
 	local Humanoid = Model:FindFirstChild("Humanoid")
 
 	local Idle = AnimationsFolder.Idle
 	local Playing = AnimationsFolder.Playing
 
+	local LoadedPlaying = Humanoid:LoadAnimation(Playing)
+	local LoadedIdle = Humanoid:LoadAnimation(Idle)
+
 	if Bool then
-		PlayInstrumentAnimation(Humanoid, Playing, Idle, 1, 0.4, Bool)
+		LoadedIdle.Looped = false
+		LoadedIdle:Stop()
+		LoadedPlaying:AdjustSpeed(SpeedTrue)
+		LoadedPlaying.Looped = true
+		LoadedPlaying:Play()
 	else
-		PlayInstrumentAnimation(Humanoid, Playing, Idle, 1, 0.4, Bool)
+		LoadedPlaying.Looped = false
+		LoadedPlaying:Stop()
+		LoadedIdle:AdjustSpeed(SpeedFalse)
+		LoadedIdle.Looped = true
+		LoadedIdle:Play()
 	end
+	
 end
-
-function ACS:PlayMusic(Song, AnimationDelay, AnimationSpeed)
-
-	if Song then
-		ACS:AddAvatar(ACS.Songs[Song][2][2], ACS.Songs[Song][2][3])
-		ACS:PlayAvatarAnimation(ACS.Songs[Song][2][1], false)
-		wait(4)
-		ACS:PlayAvatarAnimation(ACS.Songs[Song][2][1], true)
-		wait(4)
-		ACS:PlayAvatarAnimation(ACS.Songs[Song][2][1], false)
-
-	end
-
-end
-
-
 
 return ACS
