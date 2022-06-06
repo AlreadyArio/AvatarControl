6e17467676f3308155742be7b62f0c92f353b8c3
Newt <55791489+Trisxcal@users.noreply.github.com>
2022-06-06 21:50:28 +0200

    Added dynamic positions
    Positions can now be changed inside a script instead of the module.

--- AvatarControlModule.lua
+++ AvatarControlModule.lua
@@ -1,24 +1,33 @@
-local ACS = {
-	Status = {
-		Success = "Success",
-		Error = "Error",
-		Return = "Return"
-	},
-
-	Positions = {
-		Front = {-25.5, 5, 34.5},
-		FrontOR = {0, 0, 0},
-
-		Back = {-25.5, 5, 42.5},
-		BackOR = {0,0,0},
-
-		Left = {-20, 5, 38.5},
-		LeftOR = {0, 40, 0},
-
-		Right = {-32.5, 5, 40},
-		RightOR = {0, -20, 0}
-	},
+local ACS = { 
+	["positions"] = { 
+		["Left"] = { 
+			["Position"] 	= { 0, 0, 0 },
+			["Orientation"] = { 0, 0, 0 },
+		},
+		["Right"] = { 
+			["Position"] 	= { 0, 0, 0 },
+			["Orientation"] = { 0, 0, 0 },
+		},
+		["Back"] = { 
+			["Position"] 	= { 0, 0, 0 },
+			["Orientation"] = { 0, 0, 0 },
+		},
+		["Front"] = { 
+			["Position"] 	= { 0, 0, 0 },
+			["Orientation"] = { 0, 0, 0 },
+		},
+	}
 }
+
+ACS.__index = ACS
+
+function ACS.SetPositions(x)
+	local self = {
+		positions = x
+	}
+	setmetatable(self, ACS)
+	return self
+end
 
 function ACS:RemoveAvatar(Model)
 	assert(Model:IsA("Model") and Model.PrimaryPart, "Model must be a valid Player Model")
@@ -26,8 +35,8 @@ function ACS:RemoveAvatar(Model)
 	if Model then
 		local HumanoidRootPart = Model:WaitForChild("HumanoidRootPart")
 
-		HumanoidRootPart.CFrame = CFrame.new(-100000,-100000,-100000) * CFrame.Angles(0,0,0)
 		HumanoidRootPart.Anchored = true
+		HumanoidRootPart.CFrame = CFrame.new(-100000,-100000,-100000) * CFrame.Angles(0,0,0)
 	end
 
 end
@@ -35,17 +44,15 @@ end
 function ACS:AddAvatar(Model, Location)
 	assert(Model:IsA("Model") and Model.PrimaryPart, "Model must be a valid Player Model")
 	assert(type(Location) == "string", "Value must be a valid string")
-	assert(Location == "Front" or Location == "Back" or Location == "Left" or Location == "Right", "Input must be a valid location type")
 
 	if Model then
 		local HumanoidRootPart = Model.PrimaryPart
-
 		if Location then
-			HumanoidRootPart.CFrame = CFrame.new(unpack(ACS.Positions[Location]))
-			Model:SetPrimaryPartCFrame(Model.PrimaryPart.CFrame * CFrame.Angles(math.rad(ACS.Positions[Location.."OR"][1]), math.rad(ACS.Positions[Location.."OR"][2]), math.rad(ACS.Positions[Location.."OR"][3])))
+			HumanoidRootPart.CFrame = CFrame.new(unpack(self.positions[Location].Position))
+			Model:SetPrimaryPartCFrame(Model.PrimaryPart.CFrame * CFrame.Angles(math.rad(self.positions[Location].Orientation[1]), math.rad(self.positions[Location].Orientation[2]), math.rad(self.positions[Location].Orientation[3])))
 		end
 	end
-	
+
 end
 
 function ACS:PlayAvatarAnimation(Model, SpeedTrue, SpeedFalse, Bool)
@@ -75,7 +82,7 @@ function ACS:PlayAvatarAnimation(Model, SpeedTrue, SpeedFalse, Bool)
 		LoadedIdle.Looped = true
 		LoadedIdle:Play()
 	end
-	
+
 end
 
 return ACS
